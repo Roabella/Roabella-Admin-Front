b8861c57264f353dea3ea95d82a8333d8863ad8d
JayEsEichi <wlstpgns51@naver.com>
2025-07-22 17:17:09 +0900

    타입 스크립트 에러 해겨ㅛㄹ

--- src/app/announcements/edit/[id]/page.tsx
+++ src/app/announcements/edit/[id]/page.tsx
@@ -1,18 +1,20 @@
-"use client";
-
-import { useParams } from "next/navigation";
 import Layout from "@/components/layout/Layout";
 import AnnouncementEditForm from "@/components/announcements/AnnouncementEditForm";
 
-const AnnouncementEditPage = () => {
-  const params = useParams();
-  const id = params.id as string;
+interface AnnouncementEditPageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function AnnouncementEditPage({
+  params,
+}: AnnouncementEditPageProps) {
+  const { id } = await params;
 
   return (
     <Layout title="공지사항 수정">
       <AnnouncementEditForm id={id} />
     </Layout>
   );
-};
-
-export default AnnouncementEditPage;
+}
--- src/app/banners/edit/[id]/page.tsx
+++ src/app/banners/edit/[id]/page.tsx
@@ -1,18 +1,18 @@
-"use client";
-
-import { useParams } from "next/navigation";
 import Layout from "@/components/layout/Layout";
 import BannerForm from "@/components/banners/BannerForm";
 
-const EditBannerPage: React.FC = () => {
-  const params = useParams();
-  const id = params.id as string;
+interface EditBannerPageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function EditBannerPage({ params }: EditBannerPageProps) {
+  const { id } = await params;
 
   return (
     <Layout title="배너 수정">
       <BannerForm mode="edit" bannerId={id} />
     </Layout>
   );
-};
-
-export default EditBannerPage;
+}
